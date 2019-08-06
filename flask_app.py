c8882d1f521f351447259128b84daa056738226a
zhahirzulkufli <zhahirzulkufliyulan@gmail.com>
2019-08-06 18:22:13 +0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,3 +9,7 @@ def homepage():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+#@app.errorhandler(404)
+#def page_not_found(error):
+#    return render_template('page_not_found.html'), 404
